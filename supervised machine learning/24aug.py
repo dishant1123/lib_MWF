4c1cc610a26263bbbdd82ef14b3cea01dd04f39d
dishant1123 <dishantshah1417@gmail.com>
2026-08-24 12:14:18 +0530

    ....

---
+++ supervised machine learning/24aug.py
@@ -0,0 +1,19 @@
+"""
+1. read csv 
+2. check the  missing  value  , fill age  ----> with  median or  mean 
+3. convert categorical column  ----> sex 
+4. train test split
+5. create the decision tree :
+6. train model 
+7. predict
+8. accuracy
+9. feature importance : using  function of  : feature_importances_
+10. graph of  feature importance
+
+"""
+from sklearn.ensemble import RandomForestClassifier
+
+model = RandomForestClassifier(
+    
+)
+
